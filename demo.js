2bbf86c4e6a329b62888ed2e194607a67319eb65
aamirk2244 <aamirk2244@gmail.com>
2020-09-26 19:39:03 +0500

    My Website

---
+++ demo.js
@@ -0,0 +1,35 @@
+/*
+var access = 	prompt("Who made this website?");
+	var is_true = true;
+	while(is_true)
+	{
+		if(access == "aamir")
+		{
+			is_true = false;
+		}
+		else
+		{
+			access = prompt("You are fool!");
+		}
+	}
+	*/
+//let check = document.getElementByClassName('top-button');
+/*
+if(check)
+{
+	alert("Hi aamir");
+}
+else
+{
+	alert("Please try again");
+}
+*/
+<script type="text/javascript">
+			
+		var home = document.getElementById("Home");
+		console.log(home);
+		home.onclick = function(){
+			alert("This is already your home stay here if you want ");
+		}
+</script>
+
